c2540eae3346f2d591a1ce276531ddc3d130eaea
Geiden93 <geiden93@gmail.com>
2021-04-27 12:55:46 +0200

    categorie dinamiche

--- database/seeders/Category.php
+++ database/seeders/Category.php
@@ -17,19 +17,28 @@ class Category extends Seeder
 
     public function run()
     {
-        $categories = ['Moto','Macchine','Biciclette','Monopattini','Computer','Televisori','Videogiochi','Immobili','Abbigliamento','Accessori','Elettronica','Libri'];
-        $icons = ['<i class="fas fa-motorcycle"></i>','<i class="fas fa-motorcycle"></i>','<i class="fas fa-motorcycle"></i>','<i class="fas fa-motorcycle"></i>'];
-       
+        $categories = [
+        ['name'=>'Moto', 'icon'=>'<i class="fas fa-motorcycle"></i>'],
+        ['name'=>'Macchine', 'icon'=>'<i class="fas fa-car"></i>'],
+        ['name'=>'Biciclette','icon'=>'<i class="fas fa-bicycle"></i>'],
+        ['name'=>'Camper','icon'=>'<i class="fas fa-caravan"></i>'],
+        ['name'=>'Computer','icon'=>'<i class="fas fa-desktop"></i>'],
+        ['name'=>'Televisori','icon'=>'<i class="fas fa-tv"></i>'],
+        ['name'=>'Videogiochi','icon'=>'<i class="fas fa-gamepad"></i>'],
+        ['name'=>'Immobili','icon'=>'<i class="fas fa-house-user"></i>'],
+        ['name'=>'Abbigliamento','icon'=>'<i class="fas fa-tshirt"></i>'],
+        ['name'=>'Accessori','icon'=>'<i class="fas fa-glasses"></i>'],
+        ['name'=>'Elettronica','icon'=>'<i class="fas fa-mouse"></i>'],
+        ['name'=>'Libri','icon'=>'<i class="fas fa-book"></i>'],
+    ];
         foreach ($categories as $category) {
         DB::table('categories')->insert([
-            'name'=>$category,
-            
+            'name'=>$category['name'],
+            'icon'=>$category['icon'],
             ]);
-
         };
 
 
-            
 
     }
 }
