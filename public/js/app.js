9b594f2fc4be0250dbfeb520db78b6ee4ba29c52
Francesco Coviello <fcoviello73@gmail.com>
2021-04-28 15:31:12 +0200

    video header

--- public/js/app.js
+++ public/js/app.js
@@ -3111,26 +3111,7 @@ function within(min, value, max) {
   \*****************************/
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-__webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.esm.js");
-
-__webpack_require__(/*! ./script */ "./resources/js/script.js");
-
-/***/ }),
-
-/***/ "./resources/js/script.js":
-/*!********************************!*\
-  !*** ./resources/js/script.js ***!
-  \********************************/
-/***/ (() => {
-
-// var categoryWrap = document.getElementById('categoryWrap');
-// // categoryWrap.style.cursor = 'pointer';
-// categoryWrap.onclick = function() {
-//     console.log('s')
-// };
-// categoryWrap.onmouseover = function() {
-//     this.style.backgroundColor = 'red';
-// };
+__webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.esm.js"); // require('./script');
 
 /***/ }),
 
@@ -8123,7 +8104,7 @@ __webpack_require__.r(__webpack_exports__);
 /************************************************************************/
 /******/ 	// The module cache
 /******/ 	var __webpack_module_cache__ = {};
-/******/
+/******/ 	
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
 /******/ 		// Check if module is in cache
@@ -8137,17 +8118,17 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 			// no module.loaded needed
 /******/ 			exports: {}
 /******/ 		};
-/******/
+/******/ 	
 /******/ 		// Execute the module function
 /******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/
+/******/ 	
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-/******/
+/******/ 	
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = __webpack_modules__;
-/******/
+/******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/chunk loaded */
 /******/ 	(() => {
@@ -8179,7 +8160,7 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 			return result;
 /******/ 		};
 /******/ 	})();
-/******/
+/******/ 	
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	(() => {
 /******/ 		// define getter functions for harmony exports
@@ -8191,12 +8172,12 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 			}
 /******/ 		};
 /******/ 	})();
-/******/
+/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
 /******/ 	})();
-/******/
+/******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
 /******/ 	(() => {
 /******/ 		// define __esModule on exports
@@ -8207,11 +8188,11 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
 /******/ 	})();
-/******/
+/******/ 	
 /******/ 	/* webpack/runtime/jsonp chunk loading */
 /******/ 	(() => {
 /******/ 		// no baseURI
-/******/
+/******/ 		
 /******/ 		// object to store loaded and loading chunks
 /******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
 /******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
@@ -8219,19 +8200,19 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 			"/js/app": 0,
 /******/ 			"css/app": 0
 /******/ 		};
-/******/
+/******/ 		
 /******/ 		// no chunk on demand loading
-/******/
+/******/ 		
 /******/ 		// no prefetching
-/******/
+/******/ 		
 /******/ 		// no preloaded
-/******/
+/******/ 		
 /******/ 		// no HMR
-/******/
+/******/ 		
 /******/ 		// no HMR manifest
-/******/
+/******/ 		
 /******/ 		__webpack_require__.O.j = (chunkId) => (installedChunks[chunkId] === 0);
-/******/
+/******/ 		
 /******/ 		// install a JSONP callback for chunk loading
 /******/ 		var webpackJsonpCallback = (parentChunkLoadingFunction, data) => {
 /******/ 			var [chunkIds, moreModules, runtime] = data;
@@ -8254,20 +8235,20 @@ __webpack_require__.r(__webpack_exports__);
 /******/ 			}
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
-/******/
+/******/ 		
 /******/ 		var chunkLoadingGlobal = self["webpackChunk"] = self["webpackChunk"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
-/******/
+/******/ 	
 /************************************************************************/
-/******/
+/******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
 /******/ 	__webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/js/app.js")))
 /******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/css/app.css")))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
-/******/
+/******/ 	
 /******/ })()
 ;
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,132 +1,164 @@
 {{-- <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1"> --}}
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> --}}
 
 
-<x-layout>
- @if (session('status'))
-    <div class="custom-session pt-5 alert alert-success">
-    {{session('status')}}
-</div>
- @endif
+    <x-layout>
 
-
-
-<div class="margin-custom-top container text-center">
-    <div class="row justify-content-center align-items-center">
-        <h2 class="display-1">Cerca la categoria</h2>
-        @foreach ($categories as $category)
-            <div class="col-md-3 custom-card col-lg-2 my-2 mx-1 card col-6">
-                <div  class="mt-3 card-body p-0">
-                    
-                    <a class="pt-3 text-decoration-none"href="{{route('byCategory', [ $category->id, $category->name])}}"><p id="categoryWrap" class="custom-category">{{$category->name}}</p>
-                    <p class="fs-1"><i class="{{$category->icon}}"></i></p></a>
-                </div>
-            </div>
-        @endforeach
-    </div>
-</div>
-
-<div class="container">
-    <br>
-    <h4>Bootstrap 4 and CCS3 Product Cards with Transition - Techhowdy(demonguru18) - Lyoid Lopes</h2>
-	<br>
-	<div class="row" id="ads">
-    <!-- Category Card -->
-    <div class="col-md-4">
-        <div class="card rounded">
-            <div class="card-image">
-                <span class="card-notify-badge">Low KMS</span>
-                <span class="card-notify-year">2018</span>
-                <img class="img-fluid" src="https://imageonthefly.autodatadirect.com/images/?USER=eDealer&PW=edealer872&IMG=USC80HOC011A021001.jpg&width=440&height=262" alt="Alternate Text" />
-            </div>
-            <div class="card-image-overlay m-auto">
-                <span class="card-detail-badge">Used</span>
-                <span class="card-detail-badge">$28,000.00</span>
-                <span class="card-detail-badge">13000 Kms</span>
-            </div>
-            <div class="card-body text-center">
-                <div class="ad-title m-auto">
-                    <h5>Honda Accord LX</h5>
-                </div>
-                <a class="ad-btn" href="#">View</a>
-            </div>
+        @if (session('status'))
+        <div class="alert alert-success mt-5">
+            {{session('status')}}
+            {{-- <p class="alert alert-success">Annuncio inserito con successo</p> --}}
         </div>
-    </div>
-    <div class="col-md-4">
-        <div class="card rounded">
-            <div class="card-image">
-                <span class="card-notify-badge">Fully-Loaded</span>
-                <span class="card-notify-year">2017</span>
-                <img class="img-fluid" src="https://imageonthefly.autodatadirect.com/images/?USER=eDealer&PW=edealer872&IMG=CAC80HOC021B121001.jpg&width=440&height=262" alt="Alternate Text" />
-            </div>
-            <div class="card-image-overlay m-auto">
-                <span class="card-detail-badge">Used</span>
-                <span class="card-detail-badge">$28,000.00</span>
-                <span class="card-detail-badge">13000 Kms</span>
-            </div>
-            <div class="card-body text-center">
-                <div class="ad-title m-auto">
-                    <h5>Honda CIVIC HATCHBACK LS</h5>
+        @endif
+
+        {{-- VIDEO HEADER --}}
+        <header>
+            <div class="overlay"></div>
+            <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop" class="w-100 h-25">
+              <source src="/video/video1.mp4" type="video/mp4">
+            </video>
+            {{-- <div class="container h-100">
+              <div class="d-flex h-100 text-center align-items-center">
+                <div class="w-100 text-white">
+                  <h1 class="display-3 text-danger">Video Header</h1>
+                  <p class="lead mb-0">With HTML5 Video and Bootstrap 4</p>
                 </div>
-                <a class="ad-btn" href="#">View</a>
-            </div>
-        </div>
-    </div>
+              </div>
+            </div> --}}
+          </header>
 
-    <div class="col-md-4">
-        <div class="card rounded">
-            <div class="card-image">
-                <span class="card-notify-badge">Price Reduced</span>
-                <span class="card-notify-year">2018</span>
-                <img class="img-fluid" src="https://imageonthefly.autodatadirect.com/images/?USER=eDealer&PW=edealer872&IMG=USC80HOC091A021001.jpg&width=440&height=262" alt="Alternate Text" />
-            </div>
-            <div class="card-image-overlay m-auto">
-                <span class="card-detail-badge">Used</span>
-                <span class="card-detail-badge">$22,000.00</span>
-                <span class="card-detail-badge">8000 Kms</span>
-            </div>
-            <div class="card-body text-center">
-                <div class="ad-title m-auto">
-                    <h5>Honda Accord Hybrid LT</h5>
-                </div>
-                <a class="ad-btn" href="#">View</a>
-            </div>
-        </div>
-    </div>
 
-</div>
-</div>
 
-<div class="container">
-    <div class="row justify-content-center align-items-center">
-        <div class="col-md-10 col-11">
-            @foreach ($announcements as $announcement)
-            <div class="my-3 card col-md-8 col-12 offset-md-2">
-                <div class="row align-items-center justify-content-end">
-                    <div class="col-5">
-                        <img class="img-fluid"  src="https://www.peperonciniamoci.it/forum/uploads/profile/photo-15386.png" style="width: 200px; height: 200px;"alt="Card image cap">
+        <div class="margin-custom-top container text-center">
+            <div class="mt-0 row justify-content-center align-items-center">
+                <h2 class="display-1">Cerca la categoria</h2>
+                @foreach ($categories as $category)
+                <div class="col-md-3 custom-card col-lg-2 my-2 mx-1 card col-6">
+                    <div class="mt-3 card-body p-0">
+
+                        <a class="pt-3 text-decoration-none "
+                            href="{{route('byCategory', [ $category->id, $category->name])}}">
+                            <p id="categoryWrap" class="custom-category">{{$category->name}}</p>
+                            <p class="fs-1"><i class="{{$category->icon}}"></i></p>
+                        </a>
                     </div>
-                    <div class="col-7">
-                        <div class="card-body">
-                            <h3 class="card-title">{{$announcement->title}}</h3>
-                            <p class="card-text">{{$announcement->description}} </p>
-                            <p class="card-text">{{$announcement->created_at->format('Y/m/d')}} </p>
-                            <p class="card-text">{{$announcement->price}}€</p>
-                            <p class="card-text">{{$announcement->user->name}}</p>
-                            <p class="card-text">Categoria: <a href="{{route('byCategory', [ $announcement->category->id, $announcement->category->name])}}">{{$announcement->category->name}}</a></p>
+                </div>
+                @endforeach
+            </div>
+        </div>
 
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="container">
+            <br>
+            <h4>Bootstrap 4 and CCS3 Product Cards with Transition - Techhowdy(demonguru18) - Lyoid Lopes</h2>
+                <br>
+                <div class="row" id="ads">
+                    <!-- Category Card -->
+                    <div class="col-md-4">
+                        <div class="card rounded">
+                            <div class="card-image">
+                                <span class="card-notify-badge">Low KMS</span>
+                                <span class="card-notify-year">2018</span>
+                                <img class="img-fluid"
+                                    src="https://imageonthefly.autodatadirect.com/images/?USER=eDealer&PW=edealer872&IMG=USC80HOC011A021001.jpg&width=440&height=262"
+                                    alt="Alternate Text" />
+                            </div>
+                            <div class="card-image-overlay m-auto">
+                                <span class="card-detail-badge">Used</span>
+                                <span class="card-detail-badge">$28,000.00</span>
+                                <span class="card-detail-badge">13000 Kms</span>
+                            </div>
+                            <div class="card-body text-center">
+                                <div class="ad-title m-auto">
+                                    <h5>Honda Accord LX</h5>
+                                </div>
+                                <a class="ad-btn" href="#">View</a>
+                            </div>
                         </div>
                     </div>
+                    <div class="col-md-4">
+                        <div class="card rounded">
+                            <div class="card-image">
+                                <span class="card-notify-badge">Fully-Loaded</span>
+                                <span class="card-notify-year">2017</span>
+                                <img class="img-fluid"
+                                    src="https://imageonthefly.autodatadirect.com/images/?USER=eDealer&PW=edealer872&IMG=CAC80HOC021B121001.jpg&width=440&height=262"
+                                    alt="Alternate Text" />
+                            </div>
+                            <div class="card-image-overlay m-auto">
+                                <span class="card-detail-badge">Used</span>
+                                <span class="card-detail-badge">$28,000.00</span>
+                                <span class="card-detail-badge">13000 Kms</span>
+                            </div>
+                            <div class="card-body text-center">
+                                <div class="ad-title m-auto">
+                                    <h5>Honda CIVIC HATCHBACK LS</h5>
+                                </div>
+                                <a class="ad-btn" href="#">View</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="card rounded">
+                            <div class="card-image">
+                                <span class="card-notify-badge">Price Reduced</span>
+                                <span class="card-notify-year">2018</span>
+                                <img class="img-fluid"
+                                    src="https://imageonthefly.autodatadirect.com/images/?USER=eDealer&PW=edealer872&IMG=USC80HOC091A021001.jpg&width=440&height=262"
+                                    alt="Alternate Text" />
+                            </div>
+                            <div class="card-image-overlay m-auto">
+                                <span class="card-detail-badge">Used</span>
+                                <span class="card-detail-badge">$22,000.00</span>
+                                <span class="card-detail-badge">8000 Kms</span>
+                            </div>
+                            <div class="card-body text-center">
+                                <div class="ad-title m-auto">
+                                    <h5>Honda Accord Hybrid LT</h5>
+                                </div>
+                                <a class="ad-btn" href="#">View</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+        </div>
+
+        <div class="container">
+            <div class="row justify-content-center align-items-center">
+                <div class="col-md-10 col-11">
+                    @foreach ($announcements as $announcement)
+                    <div class="my-3 card col-md-8 col-12 offset-md-2">
+                        <div class="row align-items-center justify-content-end">
+                            <div class="col-5">
+                                <img class="img-fluid"
+                                    src="https://www.peperonciniamoci.it/forum/uploads/profile/photo-15386.png"
+                                    style="width: 200px; height: 200px;" alt="Card image cap">
+                            </div>
+                            <div class="col-7">
+                                <div class="card-body">
+                                    <h3 class="card-title">{{$announcement->title}}</h3>
+                                    <p class="card-text">{{$announcement->description}} </p>
+                                    <p class="card-text">{{$announcement->created_at->format('Y/m/d')}} </p>
+                                    <p class="card-text">{{$announcement->price}}€</p>
+                                    <p class="card-text">{{$announcement->user->name}}</p>
+                                    <p class="card-text">Categoria: <a
+                                            href="{{route('byCategory', [ $announcement->category->id, $announcement->category->name])}}">{{$announcement->category->name}}</a>
+                                    </p>
+
+                                    <a href="{{route('announcement.show' ,$announcement)}}"
+                                        class="btn btn-primary">Dettaglio Annuncio</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
                 </div>
             </div>
-            @endforeach
-    </div>
+        </div>
 
-</div>
 
-<hr>
-
-</x-layout>
+    </x-layout>
