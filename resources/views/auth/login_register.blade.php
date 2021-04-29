5a914ca986f12df4f709385dc097e4f6444cc2d8
Francesco Coviello <fcoviello73@gmail.com>
2021-04-29 11:19:31 +0200

    margine form login

--- resources/views/auth/login_register.blade.php
+++ resources/views/auth/login_register.blade.php
@@ -3,7 +3,7 @@
 
     <div class="container">
         <div class="row">
-          <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+          <div class="col-sm-9 col-md-7 col-lg-5 mx-auto mt-5">
             <div class="card card-signin my-5">
               <div class="card-body">
                 <h5 class="card-title text-center">Login</h5>
