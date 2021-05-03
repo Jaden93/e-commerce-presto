34573868623644d91846d431e54bcd987f9629e2
Lorenzo Aielli <lorenzoaielli@gmail.com>
2021-05-03 17:25:31 +0200

    fix vari navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,30 +1,30 @@
-
 <nav class="navbar nav-custom  navbar-expand-lg bg-main">
     <div class="container-fluid">
-        <a class=" navbar-brand text-U-brand" href="{{route('homepage')}}"><i class="fas fa-store"></i>
+        <a class=" navbar-brand text-U-brand ms-2" href="{{route('homepage')}}"><i class="fas fa-store"></i>
             Presto</a>
-        <button id="button-navbar" class="navbar-toggler hvr-icon-spin  bg-sec" type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
-            aria-label="Toggle navigation">
+        <button id="button-navbar" class="navbar-toggler hvr-icon-spin ms-auto bg-sec" type="button"
+            data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown"
+            aria-expanded="false" aria-label="Toggle navigation">
             <i class="fas  hvr-icon text-main fa-cog"></i>
         </button>
 
-
-
-        <form method="GET" action="{{route('announcement.search')}}" id="button-navbar" class="offset-md-2  me-auto custom-nav-search btn nav-link ">
-            <div class="search">
-                <div class="icon"></div>
-                <div class="input">
-                    <input type="text" placeholder="Cerca il tuo annuncio">
-                </div>
+        <form method="GET" action="{{route('announcement.search')}}" id="search-navbar"
+            class="offset-md-2  me-auto custom-nav-search btn nav-link ">
+            {{-- <div class="search">
+                    <div class="icon"></div>
+                    <div class="input">
+                        <input type="text" placeholder="Cerca il tuo annuncio">
+                    </div>
+                </div> --}}
+            <div class="d-flex custom-navbar-nav">
+                <input class="form-control w-75" type="search" placeholder="Cosa stai cercando?" aria-label="Search"
+                    name="query">
+                <button class="my-auto btn btn-outline-sec " type="submit">Cerca</button>
             </div>
-            {{-- <div class="d-flex custom-navbar-nav">
-            <input class="form-control" type="search" placeholder="Cosa stai cercando?" aria-label="Search" name="query">
-            <button class="my-auto btn btn-outline-sec " type="submit">Cerca</button> --}}
-        </div>
         </form>
 
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
+
             <ul class="align-items-center navbar-nav ms-auto">
                 <li>
                 </li>
@@ -33,8 +33,7 @@
                     <a class="nav-link active fw-bold" style="display:none;" aria-current="page"
                         href="{{route('homepage')}}">Home</a>
                     @else
-                    <a class="nav-link active fw-bold" aria-current="page"
-                        href="{{route('homepage')}}">Home</a>
+                    <a class="nav-link active fw-bold" aria-current="page" href="{{route('homepage')}}">Home</a>
                     @endif
                 </li>
                 @guest
@@ -43,11 +42,11 @@
                 </li>
                 <li class="nav-item">
                     <a class="nav-link fw-bold" href="{{route('login_register')}}">Register</a>
-                </li class="" >
+                </li class="">
                 <li class="d-flex">
 
-                @endguest
-                @auth
+                    @endguest
+                    @auth
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
                         data-bs-toggle="dropdown" aria-expanded="false">
@@ -88,14 +87,28 @@
                     @endauth
                     @else
 
-                    <a class=" custom-btn btn  "
-                        href="{{route('login_register')}}"><span id="text-nav-btn">Inserisci annuncio</span> </a>
+                    <a class=" custom-btn btn me-2 " href="{{route('login_register')}}"><span
+                            id="text-nav-btn">Inserisci annuncio</span> </a>
                     @endif
+                </li>
+                <li>
+
+                    <form method="GET" action="{{route('announcement.search')}}" id="search-navbar1"
+                        class="offset-md-2 me-auto custom-nav-search btn nav-link ">
+                        {{-- <div class="search">
+                        <div class="icon"></div>
+                        <div class="input">
+                            <input type="text" placeholder="Cerca il tuo annuncio">
+                        </div>
+                    </div> --}}
+                        <div class="d-flex custom-navbar-nav">
+                            <input class="form-control w-75" type="search" placeholder="Cosa stai cercando?"
+                                aria-label="Search" name="query">
+                            <button class="my-auto btn btn-outline-sec " type="submit">Cerca</button>
+                        </div>
+                    </form>
                 </li>
             </ul>
         </div>
     </div>
 </nav>
-
-
-
