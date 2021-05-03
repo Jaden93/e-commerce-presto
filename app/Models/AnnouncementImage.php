54141aa645cd9f51e93570ef5c3bb807d79d566c
Lorenzo Aielli <lorenzoaielli@gmail.com>
2021-05-03 13:03:49 +0200

    finita user 6

--- app/Models/AnnouncementImage.php
+++ app/Models/AnnouncementImage.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Models\Announcement;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Storage;
 
 class AnnouncementImage extends Model
 {
@@ -13,5 +14,20 @@ class AnnouncementImage extends Model
 
     public function announcement() {
         return $this->belongsTo(Announcement::class);
+    }
+
+    static public function getUrlByFilePath($filePath, $width = null, $height = null){
+        if(!$width && !$height){
+            return Storage::url($filePath);
+        }
+        $path = dirname($filePath);
+        $fileName = basename($filePath);
+        $file = "{$path}/crop{$width}x{$height}_{$fileName}";
+
+        return Storage::url($file);
+    }
+
+    public function getUrl($width = null, $height = null){
+        return AnnouncementImage::getUrlByFilePath($this->file, $width, $height);
     }
 }
